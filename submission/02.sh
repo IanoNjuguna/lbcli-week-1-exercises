44785def3a068155afc0f943a238d196994d402e
IanoNjuguna <inc3099@gmail.com>
2026-06-25 17:59:15 +0300

    grep the json output and find the exact key-value pair with our network name

--- submission/02.sh
+++ submission/02.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 # get the bitcoin node network name
-bitcoin-cli -regtest getblockchaininfo
+# grep '"chain"' filters JSON output to find the key-value pair with the text: chain
+# cut -d '"' -f 4 uses the double quote as a delimiter to split a line into 4 pieces.
+# the 4th piece is the text: regtest
+bitcoin-cli -regtest getblockchaininfo | grep '"chain"' | cut -d '"' -f 4
