793d4621401543bc0c5c6dfbc1b8044f5254ca1a
IanoNjuguna <inc3099@gmail.com>
2026-06-25 18:12:30 +0300

    start the background daemon and wait 10 seconds

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
+# start the background daemon
+bitcoind -regtest -daemon
+echo "waiting 10 seconds for the daemon to start"
+sleep 10
 # get the bitcoin node latest block height 
 bitcoin-cli -regtest getblockcount
