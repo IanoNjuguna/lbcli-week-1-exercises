ca194258e405f1fa1f358ef632053a36b5ca8cc3
IanoNjuguna <inc3099@gmail.com>
2026-06-25 22:36:48 +0300

    use -wallet rather than -rpc-wallet for compatibility with modern systems

--- submission/05.sh
+++ submission/05.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 # 1. Get an address for your wallet
-ADDR=$(bitcoin-cli -regtest -rpcwallet=builderswallet_0 getnewaddress)
+ADDR=$(bitcoin-cli -regtest -wallet=builderswallet_0 getnewaddress)
 
 # 2. Mine 101 blocks to mature the first block reward
-bitcoin-cli -regtest -rpcwallet=builderswallet_0 generatetoaddress 101 $ADDR
+bitcoin-cli -regtest -wallet=builderswallet_0 generatetoaddress 101 $ADDR
 
 # Check the total amount in the wallet.
-bitcoin-cli -regtest -rpcwallet=builderswallet_0 getbalance
+bitcoin-cli -regtest -wallet=builderswallet_0 getbalance
