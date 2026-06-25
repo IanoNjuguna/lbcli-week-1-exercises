97d5ce92be1b65c38b1abbc2770fd11a1f1b6ed3
IanoNjuguna <inc3099@gmail.com>
2026-06-25 22:30:33 +0300

    create an address, mine 101 blocks & check the amount of $BTC in the wallet

--- submission/05.sh
+++ submission/05.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/bash
+# 1. Get an address for your wallet
+ADDR=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
+
+# 2. Mine 101 blocks to mature the first block reward
+bitcoin-cli -regtest -rpcwallet=builderswallet generatetoaddress 101 $ADDR
+
 # Check the total amount in the wallet.
-bitcoin-cli -rpcwallet=builderswallet getwalletinfo
+bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
