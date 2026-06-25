93b4fff7a4f70113128f807f37075557c3937d1d
IanoNjuguna <inc3099@gmail.com>
2026-06-25 19:30:37 +0300

    generate a new legacy address

--- submission/06.sh
+++ submission/06.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # Generate a legacy address.
-bitcoin-cli -regtest -rpcwallet=builderswallet_0 getnewaddressn "" legacy
+bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress "" legacy
