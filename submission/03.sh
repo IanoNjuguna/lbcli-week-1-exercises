95c08df167cc575d31e2e0faf744ae29098c06d6
IanoNjuguna <inc3099@gmail.com>
2026-06-25 18:19:46 +0300

    loop the getblockcount command till it responds with a ping

--- submission/03.sh
+++ submission/03.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-# start the background daemon
-bitcoind -regtest -daemon
-echo "waiting 10 seconds for the daemon to start"
-sleep 10
+# loop until bitcoin-cli responds with a ping
+until bitcoin-cli -regtest getblockcount >/dev/null 2>&1; do  
+	sleep 1
+done
 # get the bitcoin node latest block height 
 bitcoin-cli -regtest getblockcount
