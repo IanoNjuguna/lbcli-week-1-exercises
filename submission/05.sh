efaec5ff1ce8ba53e23061eb2a3ad6fe73a36449
IanoNjuguna <inc3099@gmail.com>
2026-06-25 22:57:20 +0300

    redirect the output of the minted blocks to /dev/null

--- submission/05.sh
+++ submission/05.sh
@@ -6,7 +6,7 @@ bitcoin-cli -regtest createwallet "builderswallet" > /dev/null 2>&1
 ADDR=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
 
 # mine 101 blocks to mature the first block reward
-bitcoin-cli -regtest -rpcwallet=builderswallet generatetoaddress 101 $ADDR
+bitcoin-cli -regtest -rpcwallet=builderswallet generatetoaddress 101 $ADDR > /dev/null 2>&1
 
 # check the total amount in the wallet.
 bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
