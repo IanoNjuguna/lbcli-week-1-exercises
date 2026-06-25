10cfac00957bb26df467515201d34f945c50fc43
IanoNjuguna <inc3099@gmail.com>
2026-06-25 18:41:13 +0300

    get blockcount of regtest node

--- submission/03.sh
+++ submission/03.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/bash
-# loop until bitcoin-cli responds with a ping
-until bitcoin-cli -regtest getblockcount >/dev/null 2>&1; do  
-	sleep 1
-done
 # get the bitcoin node latest block height 
 bitcoin-cli -regtest getblockcount
