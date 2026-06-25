11164771700b5b8f2907ce1df5a6dfe015cde079
IanoNjuguna <inc3099@gmail.com>
2026-06-25 22:46:24 +0300

    create a wallet first before getting an address

--- submission/05.sh
+++ submission/05.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
-# 1. Get an address for your wallet
+# create wallet
+bitcoin-cli -regtest createwallet "builderswallet" > /dev/null 2>&1
+
+# get an address for the wallet
 ADDR=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
 
-# 2. Mine 101 blocks to mature the first block reward
+# mine 101 blocks to mature the first block reward
 bitcoin-cli -regtest -rpcwallet=builderswallet generatetoaddress 101 $ADDR
 
-# Check the total amount in the wallet.
+# check the total amount in the wallet.
 bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
